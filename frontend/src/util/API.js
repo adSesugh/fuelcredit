caf576f01384d706ee655ab764ee13ea889b5f8f
Sesugh Agbadu <sesughagbadu@yahoo.com>
2023-02-05 19:23:44 +0100

    sign up logic

--- frontend/src/util/API.js
+++ frontend/src/util/API.js
@@ -4,6 +4,11 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
 })
 
+instance.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest"
+instance.defaults.headers.common['X-CSRF-TOKEN'] = window.csrf_token
+instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+instance.defaults.headers.common['Access-Control-Allow-Methods'] = "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+instance.defaults.withCredentials = true;
 //instance.defaults.headers.common['Authorization'] = `Bearer ${''}`
 
 export default instance
