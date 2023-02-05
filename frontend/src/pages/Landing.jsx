3cb21546bbe682d9a596ba03df7a51744c4a1dda
Sesugh Agbadu <sesughagbadu@yahoo.com>
2023-02-05 18:45:46 +0100

    refactor landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -5,11 +5,11 @@ const Landing = () => {
     React.useEffect(() => {
         document.title = 'Home | Fuel Credit'
     }, [])
-    
+
     return (
         <div className='flex flex-col h-screen justify-center items-center text-black dark:text-white'>
             <h1 className='font-semibold text-4xl sm:text-[12px] text-center'>Welcome to Fuel Credit</h1>
-            <div className='flex space-x-1 py-3'>
+            <div className='flex xs:flex-col xs:space-y-2 sm:space-x-1 py-3'>
                 <Link to={'/signup'} className='py-2 px-4 bg-blue-700 text-white text-[12px] w-36 text-center'>Sign up an account</Link>
                 <Link to={'/signin'} className='py-2 px-4 bg-green-700 text-white text-[12px] w-36 text-center'>Sign In</Link>
             </div>
