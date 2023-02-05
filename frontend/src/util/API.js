9dad58a6ff974a59d1c61da2d8ad79c2a5f72229
Sesugh Agbadu <sesughagbadu@yahoo.com>
2023-02-05 19:28:50 +0100

    enable cor

--- frontend/src/util/API.js
+++ frontend/src/util/API.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'http://localhost:8000/api' //process.env.REACT_APP_API_URL,
 })
 
 instance.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest"
 instance.defaults.headers.common['X-CSRF-TOKEN'] = window.csrf_token
 instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-instance.defaults.headers.common['Access-Control-Allow-Methods'] = "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-instance.defaults.withCredentials = true;
+//instance.defaults.headers.common['Access-Control-Allow-Methods'] = "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+//instance.defaults.withCredentials = true;
 //instance.defaults.headers.common['Authorization'] = `Bearer ${''}`
 
 export default instance
